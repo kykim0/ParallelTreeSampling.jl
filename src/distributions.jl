0918ed9738e446e1270540194f4b3328da8b09d6
kykim <kykim144@gmail.com>
2022-04-17 17:30:23 -0700

    Move GenericDiscreteNonParamteric struct out to a separate file

---
+++ src/distributions.jl
@@ -0,0 +1,37 @@
+# Generic DiscreteNonParametric with symbol support.
+struct GenericDiscreteNonParametric
+    support::Any
+    distribution::DiscreteNonParametric
+end
+
+GenericDiscreteNonParametric(vs::T, ps::Ps) where {
+        T<:Any,P<:Real,Ps<:AbstractVector{P}} = GenericDiscreteNonParametric([v for v in vs], DiscreteNonParametric([i for i=1:length(vs)], ps))
+
+Distributions.support(d::GenericDiscreteNonParametric) = d.support
+
+Distributions.probs(d::GenericDiscreteNonParametric)  = d.distribution.p
+
+function Base.rand(rng::AbstractRNG, d::GenericDiscreteNonParametric)
+    x = support(d)
+    p = probs(d)
+    n = length(p)
+    draw = rand(rng, float(eltype(p)))
+    cp = p[1]
+    i = 1
+    while cp <= draw && i < n
+        @inbounds cp += p[i +=1]
+    end
+    return x[i]
+end
+
+function Distributions.pdf(d::GenericDiscreteNonParametric, x::Any)
+    s = support(d)
+    idx = findfirst(isequal(x), s)
+    ps = probs(d)
+    if idx <= length(ps) && s[idx] == x
+        return ps[idx]
+    else
+        return zero(eltype(ps))
+    end
+end
+Distributions.logpdf(d::GenericDiscreteNonParametric, x::Any) = log(pdf(d, x))
